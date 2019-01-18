114ceb88c0e397eb4782c2f96828936e8d12779a
Eric Foard <foard265@gmail.com>
2019-01-17 22:50:40 -0800

    lein new app

---
+++ test/internet_of_plants/core_test.clj
@@ -0,0 +1,7 @@
+(ns internet-of-plants.core-test
+  (:require [clojure.test :refer :all]
+            [internet-of-plants.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
